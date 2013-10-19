c2b8ce342fa8282fb9d77d2ea09618e9c6dc7255
Ben <visaudi@gmail.com>
2013-10-19 15:51:40 -0500

    Fixed yui_config path

--- Javascripts/yui_config.js
+++ Javascripts/yui_config.js
@@ -4,7 +4,7 @@ var YUI_config = {
 
     modules: {
 
-        'square-row-tools': '/Users/test/Documents/tictactoeBP/tic-tac-toe/Javascripts/square-row-tools.js'
+        'square-row-tools': 'Javascripts/square-row-tools.js'
     }
 
 };
